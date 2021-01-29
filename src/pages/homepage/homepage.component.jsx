c870451fc0924125cefcbc6de17a099c08b3ff51
a-as-as100 <arif.saifi@devkraft.in>
2021-01-29 23:09:27 +0530

    homepage using scss

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+ import '../homepage/homepage.styles.scss';
+const Homepage = () => (
+  <div className="directory-menu">
+      <div className='menu-items'>
+          <div className='content'>
+              <h1 className='title'> HATS</h1>
+              <span className='subtitle'>SHOP NOW</span>
+               </div>
+      </div>
+      <div className='menu-items'>
+          <div className='content'>
+              <h1 className='title'> JACKETS</h1>
+              <span className='subtitle'>SHOP NOW</span>
+               </div>
+      </div>
+      <div className='menu-items'>
+          <div className='content'>
+              <h1 className='title'> SNEAKER</h1>
+              <span className='subtitle'>SHOP NOW</span>
+               </div>
+      </div>
+      <div className='menu-items'>
+          <div className='content'>
+              <h1 className='title'>WOMENS</h1>
+              <span className='subtitle'>SHOP NOW</span>
+               </div>
+      </div>
+      <div className='menu-items'>
+          <div className='content'>
+              <h1 className='title'> MENS</h1>
+              <span className='subtitle'>SHOP NOW</span>
+               </div>
+      </div>
+  </div>
+
+)
+
+export default Homepage;
