7d4a766623fc8e2e506e856684eaf795c0e32896
a-as-as100 <arif.saifi@devkraft.in>
2021-02-05 12:17:28 +0530

    make custom button, form input,sign-in, sign-in-and-sign-up

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { React } from 'react';
 import {Route, Switch,} from 'react-router-dom';
 
 
+
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/homepage/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
  function App() {
   return (
@@ -15,6 +17,7 @@ import Header from './components/header/header.component';
       <Switch>
       <Route exact  path ='/' component ={Homepage} />
       <Route  path ='/shop' component ={ShopPage} />
+      <Route path='/signin' component ={SignInAndSignUpPage} />
       </Switch>
     </div>
     
