802325edc613070487db6dc2ee73484fd0944242
a-as-as100 <arif.saifi@devkraft.in>
2021-02-19 18:53:00 +0530

    create aart(action,reducer,types,update cart-icon,click on icon hidden or display nd mDTP

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,24 @@
  import React from 'react';
+ import {connect} from 'react-redux';
+ 
 
  import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+ 
 
- import './cart-icon.styles.scss';
+ import './cart-icon.styles.scss'; 
+ import toggleCartHidden from './../../redux/cart/cart.action';
+ 
 
-
- const CardIcon = () => (
-     <div className='cart-icon'>
+ const CartIcon = ({toggleCartHidden}) => (
+     <div className='cart-icon' onClick={toggleCartHidden}>
          <ShoppingIcon className='shopping-icon' />
          <span className='item-count'>0</span>
      </div>
  );
-
- export default CardIcon;
+  
+ const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+  });
+ export default connect(
+   null,  
+    mapDispatchToProps) (CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,7 @@ import { auth } from './../../firebase/firebase.utils';
 import CardIcon from './../cart-icon/cart-icon.component';
 import CartDropDown from './../card-dropdown/card-dropdown.component';
 
-const Header = ( {currentUser}) => (
+const Header = ( { currentUser, hidden}) => (
     <div className='header'>
      
      <Link className='logo-container' to="/">
@@ -36,15 +36,16 @@ const Header = ( {currentUser}) => (
            <CardIcon/>
 
          </div>
-      <CartDropDown/>
+         {hidden ? null : <CartDropDown/>}
 
 
     </div>
 );
 
-const mapStateToProps = state =>({
- currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
+  });
 
 
 export default connect(mapStateToProps)(Header);
