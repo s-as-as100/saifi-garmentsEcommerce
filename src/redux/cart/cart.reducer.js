4aae022f2181ce43a03ef11ffa17183921ad1c28
a-as-as100 <arif.saifi@devkraft.in>
2021-02-23 10:34:13 +0530

    remove the cartiems from the checkout item nd crt new action, reducer,types, new function mDTP

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,7 @@
 import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
+// import CartItem from './../../components/cart-item/cart-item.component';
+// import { clearItemFromCart } from './cart.action';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -18,6 +20,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
+      case CartActionTypes.CLEAR_ITEM_FROM_CART:
+        return {
+          ...state,
+          CartItems: state.cartItems.filter(cartItem => cartItem.id ===! action.payload.id)
+        }
     default:
       return state;
   }
