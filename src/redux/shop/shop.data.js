83b9049fbc1f59f87d7b1c85a3145079a972ef9b
s-as-as100 <s.arifsaifi000@gmail.com>
2021-03-01 16:46:20 +0530

    update shop data convert array into object because using of data normalization iit has advantage of inside in my application before we are using find method to find the element inside array it take to much time to iterate each element inside the array now problem is solved using the data normalization

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,6 +1,6 @@
-  const SHOP_DATA = [
+  const SHOP_DATA = {
  
-  {
+ hats: {
     id: 1,
     title: 'Hats',
     routeName: 'hats',
@@ -61,7 +61,7 @@
       }
     ]
   },
-  {
+   sneakers:{
     id: 2,
     title: 'Sneakers',
     routeName: 'sneakers',
@@ -116,7 +116,7 @@
       }
     ]
   },
-  {
+  jackets :{
     id: 3,
     title: 'Jackets',
     routeName: 'jackets',
@@ -153,7 +153,7 @@
       }
     ]
   },
-  {
+  womens:{
     id: 4,
     title: 'Womens',
     routeName: 'womens',
@@ -202,7 +202,7 @@
       }
     ]
   },
-  {
+  mens:{
     id: 5,
     title: 'Mens',
     routeName: 'mens',
@@ -245,7 +245,7 @@
       }
     ]
   }
-];
+  };
  
 
  export default SHOP_DATA;
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,14 +1,7 @@
 import { createSelector } from 'reselect';
 
 
-const COLLECTION_ID_MAP = {
-  hats :1,
-  sneakers:2 ,
-  jackets:3,
-  womens:4,
-  mens:5
-}
-
+ 
 
 
 const selectShop = state => state.shop;
@@ -26,5 +19,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    collections => collections[collectionUrlParam]
   );
