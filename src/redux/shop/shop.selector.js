e45221bd6cdbb0dbd5f21681d05f057a8a157431
s-as-as100 <s.arifsaifi000@gmail.com>
2021-03-01 15:53:35 +0530

    nested Routing in Shop Page nd collection Routing nd Selector nd memoizong select Collection Url Param

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,5 +1,16 @@
 import { createSelector } from 'reselect';
 
+
+const COLLECTION_ID_MAP = {
+  hats :1,
+  sneakers:2 ,
+  jackets:3,
+  womens:4,
+  mens:5
+}
+
+
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,5 +26,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections => collections[collectionUrlParam]
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
   );
